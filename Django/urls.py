64fce8b494f936955efdedcec1cc79bf22c545c7
DonFernando25 <fe.arevaloa@duocuc.cl>
2024-11-16 15:19:36 -0300

    Update urls.py

--- Django/urls.py
+++ Django/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from Estrella import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Estrella/',include('Estrella.urls')),
-    path('', home_view, name='home'),
+    path('', views.index, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
