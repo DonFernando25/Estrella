660faf377895923e936443726a368e499e1a79eb
DonFernando25 <fe.arevaloa@duocuc.cl>
2024-11-16 14:09:01 -0300

    Update urls.py

--- Django/urls.py
+++ Django/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Estrella/',include('Estrella.urls')),
+    path('', home_view, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
