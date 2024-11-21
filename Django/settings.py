b9ce29d6c25a7f57b736f364000733d336f465b3
DonFernando25 <fe.arevaloa@duocuc.cl>
2024-11-21 15:51:27 -0300

    Update settings.py

--- Django/settings.py
+++ Django/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Estrella', 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'Estrella', 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
